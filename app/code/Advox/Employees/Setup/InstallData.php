3943c6d4d31fb4fb35cfe84cac5750f33761f637
Mateusz <m.weszka@advoxstudio.com>
2019-09-09 14:32:38 +0200

    Added InstallData, InstallSchema

---
+++ app/code/Advox/Employees/Setup/InstallData.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Vendor\Module\Setup;
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Psr\Log\LoggerInterface;
+
+class InstallData implements InstallDataInterface
+{
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $setup;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context): void
+    {
+        try {
+            $this->setup = $setup;
+            $this->setup->startSetup();
+        } catch (\Exception $e) {
+            $this->logger->error($e->getMessage());
+        } finally {
+            $this->setup->endSetup();
+        }
+    }
+}
