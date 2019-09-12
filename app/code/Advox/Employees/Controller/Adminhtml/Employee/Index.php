dcd7564703581071e8590b826c4e991e6c0d6572
Mateusz Weszka <m.weszka@advoxstudio.pl>
2019-09-12 14:15:17 +0200

    Fixed whitespaces

--- app/code/Advox/Employees/Controller/Adminhtml/Employee/Index.php
+++ app/code/Advox/Employees/Controller/Adminhtml/Employee/Index.php
@@ -6,14 +6,15 @@ use Magento\Backend\App\Action;
 
 class Index extends Action
 {
-    private const ADMIN_RESOURCE = 'Advox_Employees::content';
+    private const ADMIN_RESOURCE = 'Advox_Employees::advox_employee_listing';
 
     public function execute(): void
     {
         echo "Hello World";
     }
 
-    protected function _isAllowed()
+
+    protected function _isAllowed(): bool
     {
         return $this->_authorization->isAllowed(self::ADMIN_RESOURCE);
     }
