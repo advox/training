59394721b1a733539f992820324ff118dd3e8bae
Mateusz Weszka <m.weszka@advoxstudio.pl>
2019-09-16 12:53:26 +0200

    Added Admin Gruid Ui component

--- app/code/Advox/Employees/Controller/Adminhtml/Employee/Index.php
+++ app/code/Advox/Employees/Controller/Adminhtml/Employee/Index.php
@@ -2,17 +2,45 @@
 
 namespace Advox\Employees\Controller\Adminhtml\Employee;
 
+use Advox\Employees\Model\EmployeeFactory;
 use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
 
 class Index extends Action
 {
-    private const ADMIN_RESOURCE = 'Advox_Employees::advox_employee_listing';
+    public const ADMIN_RESOURCE = 'Advox_Employees::advox_employee_listing';
+    /**
+     * @var Context
+     */
+    private $context;
+    /**
+     * @var PageFactory
+     */
+    private $pageFactory;
+    /**
+     * @var EmployeeFactory
+     */
+    private $employeeFactory;
 
-    public function execute(): void
-    {
-        echo "Hello World";
+    public function __construct(
+        Context $context,
+        PageFactory $pageFactory,
+        EmployeeFactory $employeeFactory
+    ) {
+        parent::__construct($context);
+        $this->context = $context;
+        $this->pageFactory = $pageFactory;
+        $this->employeeFactory = $employeeFactory;
     }
 
+    public function execute()
+    {
+        /** @var PageFactory $resultPage */
+        $resultPage = $this->pageFactory->create();
+        $resultPage->getConfig()->getTitle()->prepend(__("Advox Employees Listing"));
+        return $resultPage;
+    }
 
     protected function _isAllowed(): bool
     {
