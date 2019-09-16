feb1ab3d3a079fa51dbaae7455efe58d9507e15d
Mateusz Weszka <m.weszka@advoxstudio.pl>
2019-09-16 13:30:18 +0200

    Fixed code style Added activemenu

--- app/code/Advox/Employees/Controller/Adminhtml/Employee/Index.php
+++ app/code/Advox/Employees/Controller/Adminhtml/Employee/Index.php
@@ -10,35 +10,31 @@ use Magento\Framework\View\Result\PageFactory;
 class Index extends Action
 {
     public const ADMIN_RESOURCE = 'Advox_Employees::advox_employee_listing';
-    /**
-     * @var Context
-     */
-    private $context;
-    /**
-     * @var PageFactory
-     */
+
+    /** @var PageFactory */
     private $pageFactory;
-    /**
-     * @var EmployeeFactory
-     */
+
+    /** @var EmployeeFactory */
     private $employeeFactory;
 
     public function __construct(
         Context $context,
-        PageFactory $pageFactory,
-        EmployeeFactory $employeeFactory
+        EmployeeFactory $employeeFactory,
+        PageFactory $pageFactory
     ) {
-        parent::__construct($context);
-        $this->context = $context;
         $this->pageFactory = $pageFactory;
         $this->employeeFactory = $employeeFactory;
+
+        parent::__construct($context);
     }
 
     public function execute()
     {
         /** @var PageFactory $resultPage */
         $resultPage = $this->pageFactory->create();
+        $resultPage->setActiveMenu('Advox_Employees::employee_listing');
         $resultPage->getConfig()->getTitle()->prepend(__("Advox Employees Listing"));
+
         return $resultPage;
     }
 
